e2b23149a10b5a3ef9f545ed3c0bf92a78b0a72e
dhillonsh <dhillonsh@gmail.com>
2014-10-27 19:30:06 -0700

    Added contribution comment to top of program

--- Assignment 2.rkt
+++ Assignment 2.rkt
@@ -1,6 +1,7 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-reader.ss" "lang")((modname |Assignment 2|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+;Used sample code from class to construct project
 (require rsound)
 (require 2htdp/image)
 (require 2htdp/universe)
